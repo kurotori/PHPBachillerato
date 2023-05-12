fe135456604249983af25331eba0df2f44155d3b
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2023-05-12 18:07:04 -0300

    ...

--- API - Acceso a BdD/funciones.js
+++ API - Acceso a BdD/funciones.js
@@ -1,2 +1,27 @@
 const inptBuscar = document.getElementById("inptBuscar");
+const btnBuscar = document.getElementById("btnBuscar");
 
+
+btnBuscar.addEventListener("click",obtenerFecha);
+
+let servidor = {};
+
+function buscarLibro() {
+    titulo.innerText="Espere...";
+    setTimeout(() => {
+        fetch("fecha_hora/index.php")
+        .then(respuesta=>respuesta.text())
+        .then(datos=>{
+            servidor = JSON.parse(datos);
+            let dia = servidor.Respuesta.datos.fecha.dia;
+            let mes = servidor.Respuesta.datos.fecha.mes;
+            let anio = servidor.Respuesta.datos.fecha.anio;
+            let hora = servidor.Respuesta.datos.hora.hora;
+            let minuto = servidor.Respuesta.datos.hora.minuto;
+
+            titulo.innerText = dia+"/"+mes+"/"+anio+" "+hora+":"+minuto;
+            }
+        );
+    }, 500);
+    
+}
