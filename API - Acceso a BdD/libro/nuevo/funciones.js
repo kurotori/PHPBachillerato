310c1eac7ce1491691f8cc134079d53ea2ac3196
Luis Sebastián <luissebastianangeles@gmail.com>
2023-06-23 21:07:13 -0300

    Cambios varios. Se agrega la funcionalidad de Ingreso de datos por POST

---
+++ API - Acceso a BdD/libro/nuevo/funciones.js
@@ -0,0 +1,13 @@
+const inptTitulo = document.getElementById("inptTitulo");
+const inptISBN = document.getElementById("inptISBN");
+const inptNombreAutor = document.getElementById("inptNombreAutor");
+const inptApellidoAutor = document.getElementById("inptApellidoAutor");
+const inptGenero = document.getElementById("inptGenero");
+const inptAnio = document.getElementById("inptAnio");
+const btnGuardar = document.getElementById("btnGuardar");
+const h3Titulo = document.getElementById("titulo");
+const divCuerpo = document.getElementById("cuerpo");
+
+btnGuardar.addEventListener("click",guardar);
+
+
